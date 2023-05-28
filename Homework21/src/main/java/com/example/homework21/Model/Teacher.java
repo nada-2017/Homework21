efc102baec096c474bf62390dbc43079acbf8743
nada-2017 <132237624+nada-2017@users.noreply.github.com>
2023-05-28 21:48:18 +0300

    Add files via upload

---
+++ Homework21/src/main/java/com/example/homework21/Model/Teacher.java
@@ -0,0 +1,36 @@
+package com.example.homework21.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "varchar(20) not null")
+    private String name;
+
+    @Column(columnDefinition = "int default 0")
+    private Integer age;
+
+    @Column(columnDefinition = "varchar(30) not null")
+    private String email;
+
+    @Column(columnDefinition = "int default 0")
+    private Integer salary;
+
+    @OneToOne(cascade = CascadeType.REMOVE, mappedBy = "teacher")
+    @PrimaryKeyJoinColumn
+    private Address address;
+
+}
